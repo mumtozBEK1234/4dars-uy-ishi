fa1ec3148727ea48403d685251e5b0199e4d6bd7
mumtozBEK1234 <email manzilingiz>
2024-06-09 08:00:42 +0300

    4dars uy ishi

---
+++ my.js
@@ -0,0 +1,67 @@
+// poyezd
+// let t = + prompt ("son");
+// let T = + prompt ("son");
+// let vaqt = (t+T)%24;
+// console.log(vaqt);
+
+
+// harorat
+// let selsiy = + prompt("selsiy");
+// let kelvin = selsiy + 273.15;
+// let farangeyit = selsiy *1.80 + 32.00;
+// console.log(kelvin);
+// console.log(farangeyit);
+
+
+// ali vali
+// let ali;
+// let vali;
+// let n = + prompt("yosh");
+// let k = + prompt("martta");
+// ali = vali + n
+// vali = ali / k
+// console.log(ali);
+// console.log(vali);
+
+
+// tarvuz
+// let n = + prompt("kg");
+// let a = + prompt("%");
+// let b = + prompt("%");
+// let x = (100*n-n*a)/(100-b);
+// console.log(x);
+
+
+// burchak
+// let n = + prompt("tomonlar soni");
+// let = b=180*(n-2);
+// console.log(b);
+
+// vaqt
+// let hh = + prompt("0-24");
+// let mm = + prompt("0-60");
+// let t = + prompt("qancha vaqt otti");
+// let h = t/60;
+// h=parseInt(h);
+// let m = t%60
+// if ((h+hh)>=0 && (h+hh)<=24){
+// h=h+hh;
+// }else{
+// h=h+hh-24;
+// }
+
+// if ((m+mm)>=0 && (m+mm)<60){
+// m=m+mm;
+// }else{
+// m=m+mm-60;
+// h= h + 1
+// }
+// console.log(h,m);
+
+
+// buvi
+// let buvisi = + prompt("buvisini yoshi");
+// let sardor = + prompt("sardor yoshi");
+// let x=buvisi-2*sardor;
+// console.log(x);
+
